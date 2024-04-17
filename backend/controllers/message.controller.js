a6bc8d00fd5774a1a78e3839cd6e1371e6690390
topping <topping@mindwareworks.com>
2024-04-17 15:14:40 +0900

    0417 특정 유저가 받은 메세지들(GET)까지 완료

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,7 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 
-export const sendMessage = async (req, res) => {
+export const sendMessages = async (req, res) => {
     try {
         const {message} = req.body; // input으로 유저에게 메세지를 받으면
         const {id: receiverId} = req.params; // 받는 사람의 아이디를 params를 이용해 가져오고
@@ -30,17 +30,41 @@ export const sendMessage = async (req, res) => {
         if (newMessage) {
             conversation.messages.push(newMessage._id);
         }
-        
+
+        // Socket IO 부분
+
         // conversation 저장 후 newMessage 저장
         // await conversation.save(); // 디비에 저장
         // await newMessage.save(); // 디비에 저장
 
         // 병렬로 처리
-        await Promise.all([coversation.save(), newMessage.save()]);
+        await Promise.all([conversation.save(), newMessage.save()]);
 
         res.status(201).json(newMessage);
     } catch (error) {
-        console.error(`sendMessage 컨트롤러에서 에러 발생 : ${error.message}`);
+        console.error(`sendMessages 컨트롤러에서 에러 발생 : ${error.message}`);
         res.status(500).json({error: "Internal server error"});
     }
 };
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id: userToChatId} = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants: { $all: [senderId, userToChatId] },
+        }).populate("messages"); // Message 모델에서 실제 메시지를 가져와 Conversation 문서와 함께 반환
+        // 참조가 아닌 실제 메세지를 가져옴
+        
+        if (!conversation) return res.status(200).json([]);
+
+        const messages = conversation.messages;
+
+        res.status(200).json(messages);
+
+    } catch (error) {
+        console.error(`getMessages 컨트롤러에서 에러 발생 : ${error.message}`);
+        res.status(500).json({error: "Internal server error"});
+    }
+} 
--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendMessage } from "../controllers/message.controller.js";
+import { sendMessages, getMessages } from "../controllers/message.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/send/:id", protectRoute, sendMessage); //  protectRoute를 호출하면 next() 함수실행
+router.get("/:id", protectRoute, getMessages); //  protectRoute를 호출하면 next() 함수실행
+router.post("/send/:id", protectRoute, sendMessages); //  protectRoute를 호출하면 next() 함수실행
 
 export default router;
